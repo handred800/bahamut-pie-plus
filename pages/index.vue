9b14e9bfdd9221a7fa2b2af49d6cc6958b8206d0
handred800 <handred800@gmail.com>
2024-04-13 15:39:13 +0000

    新增 username搜尋

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,34 @@
 <template>
-    <ul>
-        <li v-for="item in list" :key="item.csn">{{ item.title }}</li>
-    </ul>
+  <form @submit.prevent="searchUser">
+    <input class="bg-slate-200" type="text" v-model="username" />
+    <button type="submit">搜尋</button>
+  </form>
+
+  <small>筆數:{{ list.length }}</small>
+  <ul>
+    <li v-for="item in list" :key="item.csn">
+      <details class="p-1 cursor-pointer">
+        <summary>{{ item.title }}</summary>
+        <textarea class="p-1 bg-slate-200" readonly>{{ item }}</textarea>
+      </details>
+    </li>
+  </ul>
 </template>
 <script setup lang="ts">
-const { data: list } = await useFetch('/api/list', {
-    query: { username: 'handred800' }
-});
+let username = ref("");
+let list = reactive([]);
+async function searchUser() {
+  const data = await $fetch("/api/list", {
+    query: { username: username.value },
+  });
+  list = data;
+  console.log(list);
+}
 </script>
+<style scoped>
+textarea {
+  width: 100%;
+  height: 250px;
+  resize: none;
+}
+</style>
